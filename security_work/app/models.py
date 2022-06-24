3aa4998e675c4d353ec1dbd16d20b1222b479dd0
Viktor <viktor.malakhov05@gmail.com>
2022-06-25 02:07:25 +0500

    Add requirements

--- security_work/app/models.py
+++ security_work/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Organisation(models.Model):
+
+    # collective agreement
+    trade_union_organisation = models.BooleanField(verbose_name="Наличие профсоюзной организации")
+    collective_agreement = models.PositiveBigIntegerField(verbose_name="Наличие коллективного договора")
+    change_agreement = models.PositiveBigIntegerField(verbose_name="Изменения в колдоговор")

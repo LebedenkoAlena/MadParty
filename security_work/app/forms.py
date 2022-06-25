ecf7e1bd392f079425531bdf363a4c81449199ad
Viktor <viktor.malakhov05@gmail.com>
2022-06-25 23:58:40 +0500

    First version of organisation form

--- security_work/app/forms.py
+++ security_work/app/forms.py
@@ -6,7 +6,9 @@ class OrganisationForm(forms.ModelForm):
     class Meta:
         model = Organisation
         fields = '__all__'
-        exclude = ['gold_sign', 'gold_sign_date',
+        exclude = ['user_id',
+                   'gold_sign',
+                   'gold_sign_date',
                    'valuated_workplace_percent',
                    'workers_on_danger_positions_percent',
                    'average_percent_provided_coveralls',
@@ -15,3 +17,104 @@ class OrganisationForm(forms.ModelForm):
                    'percent_of_educated']
 
 
+class GeneralOrgForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = ['opf',
+                  'name',
+                  'short_name',
+                  'address',
+                  'fact_address',
+                  'leader_name',
+                  'inn',
+                  'oktmo',
+                  'activity_type',
+                  'workers_number',
+                  'leader_phone',
+                  'official_email',
+                  'name_safety_specialist',
+                  'specialist_phone',
+                  'specialist_email']
+
+
+class OccupationSafetyForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = ['is_valuation_done',
+                  'report_date',
+                  'report_number',
+                  'workplace_number',
+                  'workplace_number_with_valuation',
+                  'class1_workplace_number',
+                  'class1_workers_number',
+                  'class2_workplace_number',
+                  'class2_workers_number',
+                  'class31_workplace_number',
+                  'class31_workers_number',
+                  'class32_workplace_number',
+                  'class32_workers_number',
+                  'class33_workplace_number',
+                  'class33_workers_number',
+                  'class34_workplace_number',
+                  'class34_workers_number',
+                  'class4_workplace_number',
+                  'class4_workers_number']
+
+
+class ProfessionalRiskForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = ['is_risk_valuation_done',
+                  'risk_valuation_date']
+
+
+class WorkingConditionsForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = ['workers_number_with_free_coveralls',
+                  'workers_number_with_free_disinfectants',
+                  'workers_number_with_medical_brief']
+
+
+class IndustrialInjuriesForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = ['number_of_died_workers',
+                  'died_workers_info',
+                  'number_of_hard_injury',
+                  'hard_injured_workers_info',
+                  'number_of_group_accidents',
+                  'group_accidents_info',
+                  'number_of_light_injury',
+                  'light_injured_workers_info',
+                  'number_of_micro_injury',
+                  'micro_injured_workers_info']
+
+
+class CommonDataForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = ['normative_act',
+                  'committee',
+                  'count_security_workers',
+                  'agreement_of_security_work',
+                  'cabinet_of_security_work',
+                  'cabinet_of_first_aid',
+                  'plan_of_upgrade_workers_conditions',
+                  'financing_plan',
+                  'program_of_saving_aid_of_workers']
+
+
+class LaborProtectionTrainingForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = ['count_of_workers',
+                  'timely_passage']
+
+
+class CollectiveAgreementForm(forms.ModelForm):
+    class Meta:
+        model = Organisation
+        fields = ['trade_union_organisation',
+                  'collective_agreement',
+                  'change_agreement']
--- security_work/users/views.py
+++ security_work/users/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views import View
 from .forms import UserCreateForm, UserLoginForm
+from security_work.app.forms import (GeneralOrgForm, OccupationSafetyForm,
+                                     ProfessionalRiskForm,
+                                     WorkingConditionsForm,
+                                     IndustrialInjuriesForm, CommonDataForm,
+                                     LaborProtectionTrainingForm,
+                                     CollectiveAgreementForm, OrganisationForm)
 from django.contrib.auth import get_user_model, login
+from django.contrib.auth.decorators import login_required
+from security_work.app.models import Organisation
 
 User = get_user_model()
 
@@ -18,7 +27,8 @@ def create_user(request):
         if form.is_valid():
             form.non_field_errors()
             if User.objects.filter(email=form.cleaned_data["email"]):
-                form.add_error("__all__", "Пользователь с такой почтой уже существует.")
+                form.add_error("__all__",
+                               "Пользователь с такой почтой уже существует.")
             else:
                 user = form.save(commit=False)
                 user.set_password(form.cleaned_data["password1"])
@@ -27,7 +37,8 @@ def create_user(request):
                 return redirect("/")
 
     context["form"] = form
-    return render(request, template_name="users/register.html", context=context)
+    return render(request, template_name="users/register.html",
+                  context=context)
 
 
 class UserLoginView(LoginView):
@@ -40,3 +51,25 @@ class UserLoginView(LoginView):
 
 class LogoutView(LogoutView):
     next_page = reverse_lazy("login")
+
+
+@login_required
+class PassportOrgView(View):
+    login_url = reverse_lazy('login')
+    redirect_field_name = 'login'
+    model = Organisation
+
+    def get(self, request):
+        data = {'org_forms': [GeneralOrgForm,
+                              OccupationSafetyForm,
+                              ProfessionalRiskForm,
+                              WorkingConditionsForm,
+                              IndustrialInjuriesForm,
+                              CommonDataForm,
+                              LaborProtectionTrainingForm,
+                              CollectiveAgreementForm]}
+        return render(request, 'add_organisation.html', context=data)
+
+    # def post(self, request):
+    #     data = {"org_form": OrganisationForm}
+    #     return render(request, '', context=data)

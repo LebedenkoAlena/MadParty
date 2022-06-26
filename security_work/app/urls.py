abfd007bde86dc550dd2bd3b53823a5684df8eed
Дмитрий Деркач <dima.derkach2004@gmail.com>
2022-06-26 16:47:43 +0500

    1

--- security_work/app/urls.py
+++ security_work/app/urls.py
@@ -4,6 +4,6 @@ from .views import user_lk, homepage, add_organisation
 
 urlpatterns = [
     path('lk/', user_lk, name='lk'),
-    path('addOrganisation/', add_organisation),
+    path('organizations/add/', add_organisation),
     path('', homepage),
 ]

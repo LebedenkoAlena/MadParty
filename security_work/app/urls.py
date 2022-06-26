9a2ccd6a68d5f28ca0fe2d37fc0957aef0b377e3
Viktor <viktor.malakhov05@gmail.com>
2022-06-26 16:59:42 +0500

    Add urls

--- security_work/app/urls.py
+++ security_work/app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import user_lk, homepage, add_organisation
 
-
 urlpatterns = [
     path('lk/', user_lk, name='lk'),
     path('addOrganisation/', add_organisation),
     path('', homepage),
 ]
+urlpatterns += [eval(f"path('addOrganisation/{i}', add_organisation)") for i
+                in range(8)]

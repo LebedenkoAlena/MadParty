5f8fd966ac19f5d667a5e16cf66666960ab0a185
Дмитрий Деркач <dima.derkach2004@gmail.com>
2022-06-26 21:24:04 +0500

    ready

--- security_work/app/api/urls.py
+++ security_work/app/api/urls.py
@@ -5,6 +5,6 @@ from .views import PassportAPIView, OrganisationViewSet, OrganisationPDFAPI
 
 urlpatterns = [
     path('passport/edit/', PassportAPIView.as_view(), name='passport_edit'),
-    path('xml/<int:pk>', OrganisationViewSet.as_view({'get': 'retrieve'})),
-    path('pdf/<int:pk>', OrganisationPDFAPI.as_view())
+    path('xml/<int:pk>', OrganisationViewSet.as_view({'get': 'retrieve'}), name="org-xml"),
+    path('pdf/<int:pk>', OrganisationPDFAPI.as_view(), name="org-pdf")
 ]
